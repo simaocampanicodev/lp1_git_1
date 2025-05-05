6fd992216906847379198903b268b1ee4f33d522
Nuno Fachada <faken@fakenmc.com>
2025-05-05 20:32:52 +0100

    Show a message to the user

---
+++ ShowMessages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShowMessages
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
